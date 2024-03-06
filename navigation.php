3a9f357b8553598b08741f69c23a431f7ef31344
Jack Xul <xyz1357997531@gmail.com>
2024-03-06 23:54:43 +0800

    change tab name

--- navigation.php
+++ navigation.php
@@ -4,10 +4,10 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Login") {?>active<?php }?>" href="about.php">Login_Demo</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "DataBase") {?>active<?php }?>" href="contact.php">DataBase_Demo</a>
 	  </li>
 	</ul>
 </div>
