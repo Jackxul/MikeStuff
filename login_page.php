a66b4c6399fb55b96f08e19fc642f0b60e4d4dc5
Jack Xul <xyz1357997531@gmail.com>
2024-03-07 00:18:22 +0800

    add four row in login.php

--- login_page.php
+++ login_page.php
@@ -13,10 +13,19 @@
 <div class="container" id="main-content">
     <h2>Please Login</h2>
     <form action="login.php" method="post">
-        <label for="username">User Name：</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Password：</label>
-        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="region">Region：</label>
+        <input type="text" id="region" name="region" required><br><br>
+
+        <label for="projectname">Project Name：</label>
+        <input type="text" id="projectname" name="projectname" required><br><br>
+
+        <label for="servicename">Service Name：</label>
+        <input type="text" id="servicename" name="servicename" required><br><br>
+
+        <label for="cost">Cost：</label>
+        <input type="text" id="cost" name="cost" required><br><br>
+
         <input type="submit" value="Login">
     </form>
 </div>
