16dcbc4f6d6efa58199733d5ef9a404b74761997
Jack Xul <xyz1357997531@gmail.com>
2024-03-06 23:56:37 +0800

    fix path

--- navigation.php
+++ navigation.php
@@ -4,10 +4,10 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Login") {?>active<?php }?>" href="about.php">Login_Demo</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Login") {?>active<?php }?>" href="login_page.php">Login_Demo</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "DataBase") {?>active<?php }?>" href="contact.php">DataBase_Demo</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "DataBase") {?>active<?php }?>" href="database_page.php">DataBase_Demo</a>
 	  </li>
 	</ul>
 </div>
