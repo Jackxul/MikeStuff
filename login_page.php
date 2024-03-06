de8a00a3dc8f6e226e8db4fec62f621af6692f37
Jack Xul <xyz1357997531@gmail.com>
2024-03-07 00:03:06 +0800

    set login page

--- login_page.php
+++ login_page.php
@@ -1,11 +1,16 @@
+<?php include("a_config.php");?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>登录页面</title>
+	<?php include("head-tag-contents.php");?>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>登录页面</title>
 </head>
 <body>
+
+<?php include("design-top.php");?>
+<?php include("navigation.php");?>
     <h2>欢迎登录</h2>
     <form action="login.php" method="post">
         <label for="username">用户名：</label>
@@ -14,6 +19,7 @@
         <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="登录">
     </form>
+<?php include("footer.php");?>
 </body>
 </html>
 
